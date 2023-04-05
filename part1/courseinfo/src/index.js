81be2ab3478fd486bd2d544926451248d3d40ec9
wGera <wGera96@gmail.com>
2023-04-05 12:27:38 +0200

    exercise 1.3

--- part1/courseinfo/src/index.js
+++ part1/courseinfo/src/index.js
@@ -12,10 +12,9 @@ const Header=(props)=>{
 }
 
 const Content=(props)=>{
-
   const courseContent=props.contents.map((cont,index)=>{
     return(
-      <Part key={index} part={cont.part} exercise={cont.exercise}/>
+      <Part key={index} part={cont.name} exercise={cont.exercises}/>
     )
   })
 
@@ -49,18 +48,24 @@ const Total=(props)=>{
 
 const App = () => {
   const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
+  const part1 = {
+    name: 'Fundamentals of React',
+    exercises: 10
+  }
+  const part2 = {
+    name: 'Using props to pass data',
+    exercises: 7
+  }
+  const part3 = {
+    name: 'State of a component',
+    exercises: 14
+  }
 
   return (
     <div>
       <Header course={course} />
-      <Content contents={[{part:part1,exercise:exercises1},{part:part2,exercise:exercises2},{part:part3,exercise:exercises3}]} />
-      <Total exercises={[exercises1,exercises2,exercises3]} />
+      <Content contents={[part1,part2,part3]} />
+      <Total exercises={[part1.exercises,part2.exercises,part3.exercises]} />
     </div>
   )
 }
